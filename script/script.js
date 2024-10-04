0854f1d8d3622724e958df7b1f70ddb48cdc68a8
Javier <javieralzuu@gmail.com>
2024-10-04 04:18:32 -0300

    clients donee

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    centeredSlides: true, // Agrega esta línea
-    slidesPerView: 1, // Asegura que se vea solo un slide completamente
-    spaceBetween: 20, // Ajusta el espacio entre los slides (20px en este caso)
+    centeredSlides: true,
+    slidesPerView: 1,
+    spaceBetween: 25, 
   });
