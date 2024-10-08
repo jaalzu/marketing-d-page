91f7853bdf53a54a0e7f9b431737acb59b2609ec
Javier <javieralzuu@gmail.com>
2024-10-07 21:10:51 -0300

    contact done

--- script/script.js
+++ script/script.js
@@ -16,11 +16,20 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
+    let icon = this.querySelector("i");
+
+    if(icon.style.transform === "rotate(180deg)"){
+
+      icon.style.transform = "rotate(0deg)";
+    }else{
+      icon.style.transform = "rotate(180deg)";
+      
+    }
+
+
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
